2b1c157c9f3d30449e0825b7d7816efa8bcb56cf
George Watson <spam@dendodge.me>
2012-11-27 20:46:55 +0000

    Test
    Just testing, ignore this!

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <h2>Hello World!</h2>
 		I made a pretty thing that can do calculations involving gravity. <a href="?p=gravity">Try it out!</a>
 	<h3>This is a Subheading</h3>
-		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty?
+		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty? <strong>This is a test.</strong>
 	<h3>Tip jar</h3>
 		<a class="FlattrButton" style="display:none;" href="http://dendodge.me"></a>
 		<noscript><a href="http://flattr.com/thing/947956/dendodge" target="_blank">
