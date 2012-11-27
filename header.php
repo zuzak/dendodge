8f1981468d064925a2340ee8d5b36817f52ce1bf
George Watson <spam@dendodge.me>
2012-11-27 20:50:18 +0000

    Remove testing stuff
    I don't need/want this any more

--- header.php
+++ header.php
@@ -25,19 +25,6 @@
 			}
 		}
 		?>
-		<!-- For Flattr -->
-		<script type="text/javascript">
-/* <![CDATA[ */
-(function () {
-	"use strict";
-	/*jslint browser:true */
-	var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-	s.type = 'text/javascript';
-	s.async = true;
-	s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-	t.parentNode.insertBefore(s, t);
-}());
-/* ]]> */
 		</script>
 	</head>
 	<body>
--- home.php
+++ home.php
@@ -2,8 +2,4 @@
 <h2>Hello World!</h2>
 		I made a pretty thing that can do calculations involving gravity. <a href="?p=gravity">Try it out!</a>
 	<h3>This is a Subheading</h3>
-		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty? <strong>This is a test.</strong>
-	<h3>Tip jar</h3>
-		<a class="FlattrButton" style="display:none;" href="http://dendodge.me"></a>
-		<noscript><a href="http://flattr.com/thing/947956/dendodge" target="_blank">
-		<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" /></a></noscript>
+		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty?
