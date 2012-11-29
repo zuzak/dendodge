c3239483c6af19d10b64dff8ea466c91f84e2e27
George Watson <spam@dendodge.me>
2012-11-29 19:07:50 +0000

    Update homepage
    MOAR LINCKS

--- home.php
+++ home.php
@@ -1,5 +1,9 @@
 <img src="images/me.jpg" alt="Photograph of George Watson" class="right bigimg" />
 <h2>Hello World!</h2>
-		I made a pretty thing that can do calculations involving gravity. <a href="?p=gravity">Try it out!</a>
+		I made:
+		<ul>
+			<li>A cool, but fiendishly complicated, <a href="?p=harvard">Harvard citation generator</a>.</li>
+			<li>A pretty thing that can do calculations involving gravity. <a href="?p=gravity">Try it out!</a></li>
+		</ul>
 	<h3>This is a Subheading</h3>
 		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty?
