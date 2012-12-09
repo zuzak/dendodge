9404b28e3d5883ef498b7c4af9a8563fe2ac3b9c
George Watson <spam@dendodge.me>
2012-12-09 22:12:12 +0000

    Add sitemap
    - Added PHP sitemap
    - Added XML sitemap
    - Added link to sitemap in footer
    - Improved footer accessibility by:
    -- separating external links
    -- moving stylistic elements (separating lines) to CSS

---
+++ sitemap.php
@@ -0,0 +1,16 @@
+<h2>Sitemap</h2>
+<h3>Primary pages</h3>
+<ul>
+	<li><b><a href="?p=home" rel="home">Home</a></b></li>
+	<li><a href="?p=about">About</a></li>
+	<li><a href="?p=contact">Contact</a></li>
+	<li><a href="?p=portfolio">Portfolio</a></li>
+	<li><a href="?p=privacy">Privacy policy</a></li>
+</ul>
+<h3>Projects</h3>
+<ul>
+	<li><a href="?p=gravity">Gravity calculator</a></li>
+	<li><a href="?p=harvard">Harvard citation generator</a></li>
+</ul>
+
+<a href="sitemap.xml">XML version</a>
