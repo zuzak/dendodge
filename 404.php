132cf380e9ab8616f5b0c31f128f954a304118be
George Watson <spam@dendodge.me>
2013-02-08 20:41:01 +0000

    Small fixes
    - Fix quotes in chapter example in Harvard citation generator
    - Wrap JavaScript message on 404 page in <noscript> tags, so it only
    appears to people who need it, and emphasise it so it's more obvious

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 		<li><strong><a href="/">Return to the home page</a></strong> and look for the required resource from there</li>
 		<li><strong>Use the navigation links at the top of the page</strong>, and see if one of those goes where you want to be</li>
 		<li><strong><a href="?p=sitemap">Check the sitemap</a></strong> for the resource you wanted</li>
-		<li><strong>Search for the resource</strong> using the box below (JavaScript required)</li>
+		<li><strong>Search for the resource</strong> using the box below<noscript>; <em>you'll need to enable JavaScript to do this</em></noscript></li>
 		<li>If you got here by following an internal link, please <a href="mailto:george@dendodge.me">email me</a> so I can fix it; unfortunately, there's not a lot I can do about links from other websites, but I'd be very grateful you could get in touch with that site's owner and ask them to fix it</li>
 	</ul>
 <!-- The search box below breaks validation a teeny bit, but its usefulness is more than worth it, and it degrades gracefully -->
