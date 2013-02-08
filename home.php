038ac724b3145f73e3fcd216fec021deb69c9726
George Watson <spam@dendodge.me>
2013-02-08 17:38:48 +0000

    Make the homepage more useful
    - Added seach box and Twitter feed to homepage
    
    Potential issues:
    - Twitter feed and search box only work if JS is enabled. If JS is not
    enabled, a note politely suggests that the user enable JS, but it would
    be nice to find a JS-independent solution

--- home.php
+++ home.php
@@ -5,5 +5,19 @@
 			<li>A cool, but fiendishly complicated, <a href="?p=harvard">Harvard citation generator</a>.</li>
 			<li>A pretty thing that can do calculations involving gravity. <a href="?p=gravity">Try it out!</a></li>
 		</ul>
-	<h3>This is a Subheading</h3>
-		<strong>And here is some strong text.</strong> Isn't it just <em>sooo</em> pretty?
+<a class="twitter-timeline" href="https://twitter.com/dendodge" data-widget-id="299933879438024705">Tweets by @dendodge</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<h3>Search</h3>
+<!-- The search box below breaks validation a teeny bit, but its usefulness is more than worth it, and it degrades gracefully -->
+<script>
+  (function() {
+    var cx = '015942104315057554953:vfyfj05s9gi';
+    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+<noscript>Sorry, JavaScript is required to use the search feature. Try enabling JavaScript in your browser to improve your browsing experience.</noscript>
